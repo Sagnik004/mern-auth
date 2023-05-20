13ad17c00b31df97edcca818d0ef7eba654fe8e3
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2023-05-21 01:31:40 +0530

    added custom error middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,27 @@
+// For routes not found
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// For errors occurring from the valid routes
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    statusCode = 404;
+    message = 'Resource not found';
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : null,
+  });
+};
+
+export {
+  notFound,
+  errorHandler,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+// Config dotenv
 dotenv.config();
 
+// Get port and create express app
 const port = process.env.PORT || 8080;
 const app = express();
+
+// Routes
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+// Error middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+// Start listening...
 app.listen(port, () => console.log(`Server started on port ${port}...`));
