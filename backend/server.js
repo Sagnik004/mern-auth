feb1ec610102de03f3d43493ba39cba9dd7364de
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2023-05-23 01:04:33 +0530

    register user skeleton added

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ dotenv.config();
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Allow to parse JSON and accept formdata
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 app.use('/api/users', userRoutes);
 
